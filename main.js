f33ee27b29b0cd330bce61d2cc4f31a8ec94b56c
a-bakos <bakosattila@fastem.com>
2017-01-01 21:25:15 +0000

    renamed elements in main.js

--- main.js
+++ main.js
@@ -1,39 +1,38 @@
 define(function(require, exports, module) {
 
-  var CommandManager =  brackets.getModule("command/CommandManager"),
-      Menus =           brackets.getModule("command/Menus"),
-      PanelManager =    brackets.getModule("view/PanelManager"),
-      ExtensionUtils =  brackets.getModule("utils/ExtensionUtils"),
-      AppInit =         brackets.getModule("utils/AppInit");
+  var CommandManager    = brackets.getModule("command/CommandManager"),
+      Menus             = brackets.getModule("command/Menus"),
+      PanelManager      = brackets.getModule("view/PanelManager"),
+      ExtensionUtils    = brackets.getModule("utils/ExtensionUtils"),
+      AppInit           = brackets.getModule("utils/AppInit");
 
-  var FOCUS_SHOW = "focus.show";
-  var COMMAND_ID = "focustest";
+  var INFOCUS_SHOW = "infocus.show";
+  var COMMAND_ID = "InFocus";
 
   var panel;
-  var panelHtml = require("text!focus.html");
+  var panelHtml = require("text!infocus.html");
 
-   function handle() {
-
+  function handle() {
     if(panel.isVisible()) {
       panel.hide();
-      CommandManager.get(FOCUS_SHOW);
+      CommandManager.get(INFOCUS_SHOW);
     }
     else {
       panel.show();
-      CommandManager.get(FOCUS_SHOW);
+      CommandManager.get(INFOCUS_SHOW);
     }
   }
 
-  AppInit.appReady(function () {
+  AppInit.appReady(function() {
     // Load the stylesheet:
     ExtensionUtils.loadStyleSheet(module, "style.css");
     // Register the name:
-    CommandManager.register(COMMAND_ID, FOCUS_SHOW, handle);
+    CommandManager.register(COMMAND_ID, INFOCUS_SHOW, handle);
     // Add it to menu with key combination:
     var menu = Menus.getMenu(Menus.AppMenuBar.NAVIGATE_MENU);
-    menu.addMenuItem(FOCUS_SHOW, "Ctrl-Alt-.");
+    menu.addMenuItem(INFOCUS_SHOW, "Ctrl-Alt-.");
     // Create the bottom panel:
-    panel = PanelManager.createBottomPanel(FOCUS_SHOW, $(panelHtml),200);
+    panel = PanelManager.createBottomPanel(INFOCUS_SHOW, $(panelHtml),200);
   });
 
 });
